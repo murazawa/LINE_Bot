d9ea5dc82ea728b7233712927c40eafaca380eb4
murazawa <mura.hi0115ki@gmail.com>
2021-10-22 12:41:26 +0000

    メッセージ受信確認

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,16 +1,19 @@
 class LineBotController < ApplicationController
-  protect_from_forgery except:[:callback]  
-  
+  protect_from_forgery except:[:callback]
+
   def callback
+    body = request.body.read
+    # LINEのメッセージボディ確認済み
+    # p body
   end
-  
+
   private
-  
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      
-  end
-  
+
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
+
 end
